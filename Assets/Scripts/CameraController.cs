6744f19fefc4709991befb9d8a15940f67b960a6
soppart <soppart@newschool.edu>
2020-05-05 21:23:36 -0500

    various fixes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -23,7 +23,7 @@ public class CameraController : MonoBehaviour
     {
         playerPos = player.transform.position;
         camera.transform.position = playerPos + cameraDist;
-        cameraHeight = Mathf.Pow(Move2.ballRadius, 1f) * 6;
-        cameraDist = new Vector3(0f, cameraHeight, 0f);
+        cameraHeight = Mathf.Pow(Move2.ballRadius, 1f) * 10;
+        cameraDist = new Vector3(0f, cameraHeight, -20f);
     }
 }
